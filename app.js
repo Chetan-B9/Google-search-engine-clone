5d379634758210570b636e3aa77cf89144132990
Chetan Bedakihale <chetan.bedakihale80@gmail.com>
2024-03-13 00:35:56 +0530

    ADDED: index, style, app files

---
+++ app.js
@@ -0,0 +1,36 @@
+let query = document.getElementById('query');
+let clearButton = document.getElementById('clear-btn');
+let searchButton = document.getElementById('search-btn');
+
+
+// function to handle clear button visibility on input field value changes 
+const handleClearBtnVisibility = () => {
+    if(query.value.length != 0){
+        clearButton.style.opacity = '1'
+        clearButton.style.visibility = 'visible'
+    }
+    else{
+        clearButton.style.opacity = '0'
+        clearButton.style.visibility = 'hidden'
+    }
+}
+
+const handleSearch = (e) => {
+    e.key === 'Enter' && redirector();
+  }
+  
+// Add event listener for input field 
+query.addEventListener('input', handleClearBtnVisibility);
+query.addEventListener('keypress', handleSearch);
+
+// Event listener for the Clear Button 
+clearButton.addEventListener('click', () => {
+    query.value = '';
+})
+
+
+const redirector = () => {
+    open(`https://www.google.co.in/search?q=${query.value}`,'_self');
+}
+
+
